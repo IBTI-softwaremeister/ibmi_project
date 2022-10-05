b8135985534c9ced1e67a45f221517e42a67b54e
LeeDongHyeon <ldh7228@gmail.com>
2022-10-05 21:26:46 +0900

    📄 :: (#15)[App] Add Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import { GlobalStyle } from "./styles/styles";
-import Main from "./components/page/main/index"
-import Type from "./components/page/type/index"
+import Main from "./components/page/main/index";
+import Type from "./components/page/type/index";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/common/header";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path='/' element={<Main />} />
-      <Route exact path='/type' element={<Type />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/type" element={<Type />} />
+      </Routes>
+    </>
   );
 }
 
