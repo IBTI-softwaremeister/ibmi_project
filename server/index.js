646773498b298562e7d792f47cee35a0f92f3fd6
LeeDongHyeon <ldh7228@gmail.com>
2022-10-06 01:35:00 +0900

    ⚡ :: (#31)[Socket] Update join_room

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   socket.on("join_room", (data) => {
-    socket.join(data);
+    socket.join(data.room);
   });
 
   socket.on("send_message", (data) => {
