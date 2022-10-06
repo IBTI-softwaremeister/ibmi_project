c163ef5fa0c70984787fbfb494ef76c236138834
LeeDongHyeon <ldh7228@gmail.com>
2022-10-06 13:52:59 +0900

    📄 :: (#50)[ChatRoom] Update ChatRoom Design

---
+++ client/src/components/common/chatRoom/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  width: 1264px;
+  height: 113px;
+  display: flex;
+  align-items: center;
+  background-color: #eff6d2;
+  justify-content: space-between;
+  padding-left: 60px;
+  padding-right: 27px;
+  border-radius: 20px;
+  margin-bottom: 28px;
+  font-weight: bold;
+
+  span {
+    font-size: 30px;
+  }
+
+  button {
+    border: none;
+    outline: none;
+    width: 289px;
+    height: 68px;
+    color: #fff;
+    background-color: #32b156;
+    font-size: 26.6px;
+    font-weight: bold;
+    border-radius: 17px;
+  }
+`;
